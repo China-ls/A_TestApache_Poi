0bca5430ca8dd09cb92220002f1b9ac88022a8a7
China-ls <414057419@qq.com>
2014-09-11 09:52:31 +0800

    First test from eclipse

---
+++ ExportData/src/org/ls/exportdata/DemoActivity.java
@@ -0,0 +1,16 @@
+package org.ls.exportdata;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DemoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+		
+	}
+	
+}
